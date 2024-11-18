6ec71b3be61a9040c2d5267048742a2356c6c074
Conner Hayes <34945659+cohayes@users.noreply.github.com>
2024-11-18 19:34:28 +0900

    Update _siteConfig.js
    Feature image on homepage updated.

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -11,7 +11,7 @@ import theCMS from '../cms/netlify/cms'
 export default {
   siteName: 'Conner Hayes',
   tagline: 'An Official Site and Showcase',
-  featureImage: '/uploads/home-hero.jpg',
+  featureImage: '/uploads/sunset_unk.jpeg',
   logo: 'logo-component', // 'logo-component', // or '/logo.svg' for regular image
   googleAnalytics: {
     on: true,
