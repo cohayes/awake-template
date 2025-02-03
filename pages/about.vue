2ac74f9fe82c707909cb7c1f4cb667ab70694ba0
Conner Hayes <34945659+cohayes@users.noreply.github.com>
2025-02-03 23:55:29 +0900

    Update about.vue
    About page updates

--- pages/about.vue
+++ pages/about.vue
@@ -1,88 +1,37 @@
 <template>
   <div id="about-page" class="page-wrapper about-page content-page">
     <site-hero
-      title="About Us"
-      subtitle="Example About Page"
-      image="/uploads/about-hero.jpg"
+      title="Who dis?"
+      subtitle=""
+      image="/uploads/vaina.png"
     ></site-hero>
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-          <h3>Fake Heading</h3>
+          <h3>Howdy</h3>
+
           <p>
-            <strong>
-              Edit this page in<code>/pages/about.vue</code>to fit your needs.
-            </strong>
+            I'm a gaming enthusiast with a passion for tabletop and PC games. The ability
+            for these mediums to tell stories in worlds that parallel our own is what draws
+            me to them. As a youth I was an avid reader, enjoying works of sci-fi and fantasy
+            from authors like Robert Heinlein, Ben Bova, Robert Asprin, Lynn Abbey, and Gene 
+            Wolfe. As my palate matures, I find greater enjoyment from classic authors as John
+            Steinbeck, Thomas Wolfe, and Henry Miller.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
+            My working career has revolved around geospatial data, being formally educated in
+            GIS and Geography from the University of Washington. I have held geo-data curator
+            positions at Apple and Microsoft. Additionally, I've worked on drafts for civil 
+            engineers and maps for startups. My experience has not always been so office-centric,
+            I greatly value the time I've spent working in a kitchen, manufacturing prescription
+            glasses, and teaching English language in Japan.
+          </p>
+          <p>
+            My current objectives are to learn and contribute to the global community.
           </p>
 
-          <h3>Another Fake Heading</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
-          </p>
-          <quote-card />
-          <h3>Still More Fake Content</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit! Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit.
-            <strong>Alias aut facere harum ipsam</strong> quibusdam quidem
-            quisquam saepe? Alias architecto blanditiis consequatur consequuntur
-            dicta ipsa iste modi provident, quod, similique veritatis.
-          </p>
         </div>
 
-        <div class="tile is-ancestor">
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                Cool Stuff We Do
-              </p>
-              <p class="subtitle">
-                Sooo Cool
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                Other Cool Things
-              </p>
-              <p class="subtitle">
-                Just as cool
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-          <div class="tile is-parent">
-            <article class="tile is-child box">
-              <p class="title">
-                All the cool
-              </p>
-              <p class="subtitle">
-                that is all
-              </p>
-              <figure class="image is-1by1">
-                <opti-image width="500" height="500" />
-              </figure>
-            </article>
-          </div>
-        </div>
       </template>
 
       <template v-slot:sidebar>
