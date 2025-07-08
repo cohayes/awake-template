69bc5dbbb498b7363c10bfe3a5818720f232922d
Conner Hayes <34945659+cohayes@users.noreply.github.com>
2025-07-08 22:47:10 +0900

    Update about.vue
    About me update.

--- pages/about.vue
+++ pages/about.vue
@@ -8,26 +8,36 @@
     <main-section theme="sidebar-right">
       <template v-slot:default>
         <div class="content">
-          <h3>Howdy</h3>
+          <h3>Hello There</h3>
 
           <p>
-            A gaming enthusiast with a passion for tabletop and PC games. Relatable characters
-            and conflicts draw me to these fictional worlds, where challenges can be solved by
-            combining imagination and strategy. As a youth I read avidly, enjoying science fiction and fantasy
-            from the likes of Robert Heinlein, Ben Bova, Robert Asprin, Lynn Abbey, and Gene 
-            Wolfe. As an adult, I enjoy searching out classics from authors as John
-            Steinbeck, Thomas Wolfe, and Henry Miller.
+            I'm passionate about well written stories and game design. Fictional spaces with 
+            empathetic characters can immerse the reader/player in the challenges of that world.
+            A participant's problem solving skills will develop whenever they begin to imagine potential 
+            solutions and outcomes. This development is important to the solving of our real-world problems,
+            as possible courses of action are often limited by our conceptual ability. A solution
+            the general public believes to be possible will correlate with the solutions shown in popular media.
+            Therefore I believe it is important to emphasis cooperation and non-violent problem solving in
+            the work of any creative artist.
           </p>
           <p>
-            My working career has centered on geospatial data, with a degree in Geography
-            from the University of Washington. I have held geo-data curator
-            positions at Apple and Microsoft. Additionally, I've worked for civil 
-            engineering and startup companies. My experience has not always been so office-centric though,
-            I greatly value my time spent in a kitchen, manufacturing 
-            glasses, and teaching English in Japan.
+            As a youth I was an avid reader, delving into the bodies of work by such authors as
+            Robert Heinlein, Ben Bova, Robert Asprin, Lynn Abbey, and Gene Wolfe. As an adult,
+            I have begun to enjoy searching out classic writing from authors like John
+            Steinbeck, Thomas Wolfe, and Henry Miller. The clear trend is that I began with a taste for
+            fantasy and science fiction, and have developed towards an appreciation for historical fiction.
           </p>
           <p>
-            My current objective is to learn and apply my knowledge to problems facing the global community.
+            In my working career, I strive towards positions of greater challenge and responsibility.
+            Formally educated in Geography from the University of Washington (with a strong background in science
+            and mathematics), work with geo-spatial data is at the core of my accomplishments thus far. 
+            Through vendor positions, I have curated geographic information for both Apple and Microsoft.
+            Some of my additional experiences include working for civil engineers and a GIS startup. Also
+            valuable is my time working in a kitchen, manufacturing perscription glasses, and teaching English in Japan.
+          </p>
+          <p>
+            My objective is to develop skills and experience which will contribute to solving the problems faced
+            in our global community.
           </p>
 
         </div>
