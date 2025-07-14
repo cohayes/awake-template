d37ff1c283c8de545ffa78215a99d87ed7ab341d
Conner Hayes <34945659+cohayes@users.noreply.github.com>
2025-07-14 17:37:38 +0900

    Update SiteLogo.vue

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -31,7 +31,7 @@ export default {
   },
   mounted() {
     setInterval(() => {
-      const rand = Math.ceil(Math.random() * 50)
+      const rand = Math.ceil(Math.random() * 10)
       this.blinking = rand % 2
     }, 1000)
   }
@@ -58,7 +58,7 @@ export default {
       max-height: initial;
     }
     &.blink {
-      animation: logoBlink 3s steps(6, end) alternate;
+      animation: logoBlink 0.2s steps(8, end) alternate;
     }
   }
 }
