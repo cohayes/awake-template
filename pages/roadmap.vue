8a2a0a8cd1d25d0be2e956da747c7b514c266ea5
Conner Hayes <34945659+cohayes@users.noreply.github.com>
2025-07-23 23:17:47 +0900

    Create roadmap.vue
    Using about.vue as template, created a Roadmap page.

---
+++ pages/roadmap.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="roadmap-page" class="page-wrapper roadmap-page content-page">
+    <site-hero
+      title="Upcoming Features"
+      subtitle=""
+      image="/uploads/vaina.png"
+    ></site-hero>
+    <main-section theme="sidebar-right">
+      <template v-slot:default>
+        <div class="content">
+          <h3>What's ahead</h3>
+
+          <p>
+            Probably a lot of good things!
+          </p>
+          
+        </div>
+
+      </template>
+
+      <template v-slot:sidebar>
+        <h3 class="subtitle is-4">
+          Latest Posts
+        </h3>
+        <posts-grid :per-row="1" :number="2" />
+      </template>
+    </main-section>
+  </div>
+</template>
+<script>
+import QuoteCard from '~/components/cards/QuoteCard'
+export default {
+  head() {
+    return {
+      title: `Roadmap | ${this.$siteConfig.siteName}`
+    }
+  },
+  components: { QuoteCard }
+}
+</script>
